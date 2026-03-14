06a49ecf019743fd027d2e88fe211de2891bb13e
Ezequiel Oliver <ezequiel.oliver@live.com.ar>
2026-03-14 18:02:09 -0300

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+// norma: 400, negrita: 700
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+});

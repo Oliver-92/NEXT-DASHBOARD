e6127d21f7eaaf48f595b6c94af3fd73998bc024
Ezequiel Oliver <ezequiel.oliver@live.com.ar>
2026-03-19 18:30:27 -0300

    Chapter 12: Handling Errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -25,10 +25,15 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
     
-    await sql`
+    try {
+      await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+    } catch (error) {
+      console.error('Database Error:', error);
+      throw new Error('Failed to create invoice.');
+    }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
@@ -48,11 +53,16 @@ export async function updateInvoice(id: string, formData: FormData) {
  
   const amountInCents = amount * 100;
  
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to update invoice.');
+  }
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
